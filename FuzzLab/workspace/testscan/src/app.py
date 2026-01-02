67f17c2ef96011eeb4e38eade02cc9e0dd3476dc
sonotri_250 <163321686+sonotri@users.noreply.github.com>
2026-01-02 18:13:36 +0900

    Delete FuzzLab directory

--- FuzzLab/workspace/testscan/src/app.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-def bad(user_input):
-    conn = sqlite3.connect("test.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users WHERE name = '%s'" % user_input)
-    return cur.fetchall()
